a9747584a21666f7a65bcc5b074bd613872537cc
Nikolay <nchernov@gmail.com>
2020-04-14 14:32:40 +0300

    catch smtp exception

--- work_mailer.py
+++ work_mailer.py
@@ -1,7 +1,6 @@
 import smtplib
 from mailer import Mailer
 from logger import log
-from simplebot import SimpleBot
 
 
 class WorkMailer(Mailer):
@@ -9,17 +8,22 @@ class WorkMailer(Mailer):
         super().__init__(name, email, message)
 
     def send(self):
-        log(log.INFO, 'SMTP connect to %s:%d', self.conf['mailer']['SMTP']['server'], self.conf['mailer']['SMTP']['port'])
-        with smtplib.SMTP(self.conf['mailer']['SMTP']['server'], self.conf['mailer']['SMTP']['port']) as server:  # Connect to the server
-            server.starttls()  # Use TLS
-            server.login(self.conf['mailer']['from_email'], self.conf['mailer']['passw'])  # Login to the email server
-            log(log.INFO, 'send email')
-            log(log.DEBUG, 'from: %s', self.conf['mailer']['from_email'])
-            log(log.DEBUG, 'to: %s', self.conf['mailer']['to_email'])
-            recipients = self.conf['mailer']['to_email'] + self.conf['mailer']['cc_mails']
-            server.sendmail(from_addr=self.conf['mailer']['from_email'],
-                            to_addrs=recipients,
-                            msg=self.msg.as_string())
-            log(log.DEBUG, 'e-mail sent.')
-            server.quit()  # Logout of the email server
-
+        try:
+            log(log.INFO, 'SMTP connect to %s:%d',
+                self.conf['mailer']['SMTP']['server'], self.conf['mailer']['SMTP']['port'])
+            # Connect to the server
+            with smtplib.SMTP(self.conf['mailer']['SMTP']['server'], self.conf['mailer']['SMTP']['port']) as server:
+                server.starttls()  # Use TLS
+                # Login to the email server
+                server.login(self.conf['mailer']['from_email'], self.conf['mailer']['passw'])
+                log(log.INFO, 'send email')
+                log(log.DEBUG, 'from: %s', self.conf['mailer']['from_email'])
+                log(log.DEBUG, 'to: %s', self.conf['mailer']['to_email'])
+                recipients = self.conf['mailer']['to_email'] + self.conf['mailer']['cc_mails']
+                server.sendmail(from_addr=self.conf['mailer']['from_email'],
+                                to_addrs=recipients,
+                                msg=self.msg.as_string())
+                log(log.DEBUG, 'e-mail sent.')
+                server.quit()  # Logout of the email server
+        except smtplib.SMTPException as e:
+            log(log.ERROR, 'SMTP Error: %s', e)
