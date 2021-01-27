7b47d179f38af7c5ae4b5d57459e842045f0d18a
tsyboom <tsyboom@gmail.com>
2021-01-27 12:55:58 +0200

    fix tests

--- ses.py
+++ ses.py
@@ -5,8 +5,8 @@ from flask import render_template
 def send_email(app, data, subject, att, cfg):
     mail = Mail(app)
     msg = Message(subject=subject,
-                  sender=cfg["ses"]["SENDER"],
-                  recipients=[cfg["ses"]["RECEIVER"], ])
+                  sender=cfg.conf["ses"]["SENDER"],
+                  recipients=[cfg.conf["ses"]["RECEIVER"], ])
     msg.html = render_template("contact_email.html", data=data)
     if att:
         msg.attach(att.filename, att.content_type, att.read())
